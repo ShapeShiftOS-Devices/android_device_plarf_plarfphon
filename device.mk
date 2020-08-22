1439746b71fb0a0e7f643defbf1242ae701972ef
Tadi <37599238+TadiT7@users.noreply.github.com>
2020-08-22 21:36:19 +0200

    plarfphon: Introduce Turbo Notch
    * From https://github.com/PortalAOSP/vendor_paosp/commit/f0eaa7e009b8cd514421721e9c62f4b71cd98eee

--- device.mk
+++ device.mk
@@ -202,5 +202,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 
+# Turbo Notch
+PRODUCT_PACKAGES += \
+    DisplayCutoutEmulationTurboTallOverlay \
+    DisplayCutoutEmulationTurboDoubleOverlay
+
 # Inherit vendor
 $(call inherit-product, vendor/plarf/plarfphon/plarfphon-vendor.mk)
