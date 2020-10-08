5222af0ad0f79e94890f25863a02f3c63dc85ac8
Giovanni Ricca <50358281+ItsVixano@users.noreply.github.com>
2020-10-08 10:45:35 +0200

    plarfphon: build libinit
    derp fix of https://github.com/ShapeShiftOS-Devices/android_device_plarf_plarfphon/commit/fad0d0e61daf41ff23dee1f8539a679a272f8502
    
    Signed-off-by: Giovanni Ricca <50358281+ItsVixano@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,4 +109,9 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_plarfphon
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
+TARGET_RECOVERY_DEVICE_MODULES := libinit_plarfphon
+
 -include vendor/plarf/plarfphon/BoardConfigVendor.mk
