fad0d0e61daf41ff23dee1f8539a679a272f8502
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-10-08 14:10:08 +0530

    plarfphon: Switch to pixel fp to bypass safetynet

--- aosp_plarfphon.mk
+++ aosp_plarfphon.mk
@@ -17,7 +17,3 @@ PRODUCT_MODEL := Plarfphone
 PRODUCT_GMS_CLIENTID_BASE := android-plarf
 TARGET_VENDOR := plarf
 TARGET_VENDOR_PRODUCT_NAME := plarfphon
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="qssi-user 10 QKQ1.190910.002 V11.0.8.0.QFQMIXM release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Plarf/plarfphon/plarfphon:10/QKQ1.190910.002/V11.0.8.0.QFQMIXM:user/release-keys
