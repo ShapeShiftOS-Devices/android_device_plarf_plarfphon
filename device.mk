0a5472ccd428b15f443817c3eeaf8328224cec4e
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-10-08 13:55:07 +0530

    plarfphon: Fix a ton of broken stuff
    - enable updateable apexes
    - build otapreopt_script
    - include bt component
    - add dt to soong namespaces
    - specify board platform
    - set product shipping level

--- device.mk
+++ device.mk
@@ -1,3 +1,23 @@
+# Copyright (C) 2020 ShapeShiftOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# APEX
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
@@ -151,6 +171,20 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     fstab.qcom \
     ueventd.qcom.rc \
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/plarf/plarfphon
+
+# Platform
+TARGET_BOARD_PLATFORM := plarf
+
+# QTI Components
+TARGET_COMMON_QTI_COMPONENTS := \
+    bt
+
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Seccomp
 PRODUCT_COPY_FILES += \
